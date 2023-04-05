b03ea97d1f6502e431f78246411f6a709eacc9b1
Igor <igoraraujobda@gmail.com>
2023-04-05 13:15:52 -0300

    requisito 7

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -130,13 +130,12 @@ INSERT INTO SpotifyClone.users (user_id, user_name, user_age, plan_id, plan_data
 	(3, 1),
 	(1, 2),
 	(3, 2),
-	(3, 3),
+	(2, 3),
 	(4, 4),
-	(4, 5),
 	(5, 5),
+	(6, 5),
 	(1, 6),
 	(6, 6),
 	(6, 7),
-	/* (null, 8), */
 	(3, 9),
 	(2, 10)
